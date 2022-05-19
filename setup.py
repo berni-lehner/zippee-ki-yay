9dd9066db503a6bc09a4e20de8da14252cacbf6e
Bernhard Lehner <71018765+berni-lehner@users.noreply.github.com>
2022-05-19 10:22:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='zippeekiyay',
+    name='zippee-ki-yay',
     version='0.1.1',
     author='Bernhard Lehner',
     author_email='berni.lehner@gmail.com',
