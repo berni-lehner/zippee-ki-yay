603c91e8fbcae72f2d73c7f76a8b962b31eca98c
Bernhard Lehner <71018765+berni-lehner@users.noreply.github.com>
2023-02-04 17:50:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='zippee-ki-yay',
-    version='0.1.1',
+    version='0.1.2',
     author='Bernhard Lehner',
     author_email='berni.lehner@gmail.com',
     description='Python interfaces to extract the namelist from a zip archive without the need to download it first.',
